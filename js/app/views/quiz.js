3de54f0ab1e21198dea2cb11efb62eabae8be566
indrajeet ambadekar <indrajeet.a@freighttiger.com>
2018-01-28 13:51:44 +0530

    no questions handler added

--- js/app/views/quiz.js
+++ js/app/views/quiz.js
@@ -6,8 +6,10 @@ define(function(require) {
     _ = require('underscore'),
     Backbone = require('backbone'),
     tpl = require('text!tpl/quiz/quiz.html'),
+    empty_tpl = require('text!tpl/quiz/empty.html'),
 
-    template = _.template(tpl);
+    template = _.template(tpl),
+    empty_tpl = _.template(empty_tpl);
 
   return Backbone.View.extend({
     events: {
@@ -18,14 +20,24 @@ define(function(require) {
       String.prototype.capitalize = function() {
         return this.charAt(0).toUpperCase() + this.slice(1);
       }
-      this.data = {
-        currentUser: localStorage.currentUserName,
+      if (localStorage.questions == undefined) {
+        this.data = {
+          currentUser: localStorage.currentUserName,
+        }
+        this.$el.html(empty_tpl(this.data));
+
+      } else {
+        this.data = {
+          currentUser: localStorage.currentUserName,
+        }
+        if (localStorage.questionMap) {
+          var questionMap = JSON.parse(localStorage.questionMap);
+        }
+
+        this.data.questions = this.getUserQuestions(questionMap)
+        this.$el.html(template(this.data));
       }
-      if (localStorage.questionMap) {
-        var questionMap = JSON.parse(localStorage.questionMap);
-      }
-      this.data.questions = this.getUserQuestions(questionMap)
-      this.$el.html(template(this.data));
+
       return this;
     },
     getUserQuestions: function(arr) {
